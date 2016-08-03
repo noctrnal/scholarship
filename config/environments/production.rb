7d2f5308d9ae982b3159415ad18b8ac121d58fcb
Jimmy Pearson <pearson@bagley.msstate.edu>
2016-08-02 20:35:34 -0500

    Set S3 endpoint

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,6 +101,9 @@ Rails.application.configure do
   }
 
   config.paperclip_defaults = {
+    url: ':s3_domain_url',
+    path: '/:class/:attachment/:id_partition/:style/:filename',
+    s3_host_name: 's3-us-west-2.amazonaws.com',
     storage: :s3,
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
