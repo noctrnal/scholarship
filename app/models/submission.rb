9108adfda6050f9937cf7ea4f016c52a8f57fb80
Jimmy Pearson <pearson@bagley.msstate.edu>
2016-08-31 23:02:34 -0500

    Add required and optional designations on submissions

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -22,6 +22,47 @@ class Submission < ApplicationRecord
   validates_attachment :license, content_type: { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
   validates_attachment :resume, content_type: { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
 
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :permanent_address, :presence => true
+  validates :permanent_city, :presence => true
+  validates :permanent_state, :presence => true
+  validates :permanent_zip, :presence => true
+  validates :permanent_phone, :presence => true
+  validates :citizen, :presence => true
+  validates :field_id, :presence => true
+  validates :degree_id, :presence => true
+  validates :institution_name, :presence => true
+  validates :institution_city, :presence => true
+  validates :institution_state, :presence => true
+  validates :acceptance, :presence => true
+  validates :last_year, :presence => true
+  validates :summer_tuition, :presence => true
+  validates :summer_books, :presence => true
+  validates :summer_board, :presence => true
+  validates :summer_other, :presence => true
+  validates :summer_hours, :presence => true
+  validates :fall_tuition, :presence => true
+  validates :fall_books, :presence => true
+  validates :fall_board, :presence => true
+  validates :fall_other, :presence => true
+  validates :fall_hours, :presence => true
+  validates :winter_tuition, :presence => true
+  validates :winter_books, :presence => true
+  validates :winter_board, :presence => true
+  validates :winter_other, :presence => true
+  validates :winter_hours, :presence => true
+  validates :spring_tuition, :presence => true
+  validates :spring_books, :presence => true
+  validates :spring_board, :presence => true
+  validates :spring_other, :presence => true
+  validates :spring_hours, :presence => true
+  validates :frequency, :presence => true
+  validates :motivation, :presence => true
+  validates :license, attachment_presence: true
+  validates :resume, attachment_presence: true
+  validates :expense, attachment_presence: true
+
   def field
     Field.find(degree_id)
   end
