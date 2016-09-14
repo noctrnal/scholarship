022ca60d4838af166aba01ea637561e7e17e21a2
Jimmy Pearson <pearson@bagley.msstate.edu>
2016-09-14 01:53:32 -0500

    Update environment for staging

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,24 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'scholarship-staging.hrokuapp.com',
+    :enable_starttls_auto => true,
+  }
+
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    },
+  }
 end
