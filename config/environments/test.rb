e9da1a9d8c758742d55d848f89911511038162d8
Jimmy Pearson <pearson@bagley.msstate.edu>
2016-09-14 02:09:33 -0500

    Debug emails on staging

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,6 +40,8 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.default_url_options = { :host => 'scholarship-staging.heroku.com' }
 
   ActionMailer::Base.smtp_settings = {
