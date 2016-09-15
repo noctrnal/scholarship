388d5c07d9273c4cdf1e1bf34d3598bbdf7e92d9
Jimmy Pearson <pearson@bagley.msstate.edu>
2016-09-14 23:51:36 -0500

    Updates and bug fixes
    - update content on pages#about
    - fix bug with :next_year being required
    - fields on submission on turn red when in error
    - instructions on whole numbers added for tuition fields
    - fix display bug with dashboard#home and root_url in firefox
    - distinction made for submit vs update submission in mailer
    - fix display bug with dashboard#home in firefox
    - fix display bug with recommendations#edit in firefox

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def field_class(resource, field_name)
+    if resource.errors[field_name]
+      return "error".html_safe
+    else
+      return "".html_safe
+    end
+  end
 end
+
