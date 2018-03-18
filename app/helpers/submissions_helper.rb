c200573b93fe2716e37d90b3d3dbcf0444d5af67
Jimmy Pearson <noctrnal@gmail.com>
2018-03-18 03:40:26 -0500

    Allow committee to view previous year information for reapplicants

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -2,4 +2,10 @@ module SubmissionsHelper
   def name(submission)
     "#{submission.last_name}, #{submission.first_name}"
   end
+
+  def previous_submission(submission)
+    previous_year = Setting.year - 1
+
+    Submission.find_by(:user => submission.user, :year => previous_year)
+  end
 end
